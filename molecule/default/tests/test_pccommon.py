4429105f68d2fc9e394f54763be671b01b5b6045
John Duarte <john.duarte@rackspace.com>
2018-06-14 10:47:36 -0700

    MAINT - Move teardown steps into teardown method

--- molecule/default/tests/test_pccommon.py
+++ molecule/default/tests/test_pccommon.py
@@ -10,6 +10,19 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('os-infra_hosts')[:1]
 
 
+def teardown_module(module):
+    utility_container = ("lxc-attach -n $(lxc-ls -1 | grep utility | head -n 1) "
+                         "-- bash -c '. /root/openrc ; ")
+    infra1 = testinfra.utils.ansible_runner.AnsibleRunner(
+        os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('os-infra_hosts')[0]
+    host = testinfra.get_host(infra1)
+    print ("teardown_module   module:%s on host:%s" % (module.__name__, host))
+    flavors = ['m1.tiny', 'm1.small', 'm1.medium', 'm1.large', 'm1.xlarge']
+    for flavor in flavors:
+        cmd = "{} openstack flavor delete {}'".format(utility_container, flavor)
+        host.run(cmd)
+
+
 @pytest.mark.test_id('ca13af99-5f93-11e8-8d43-6c96cfdb252f')
 @pytest.mark.jira('ASC-537')
 def test_pccommon(host):
@@ -21,12 +34,3 @@ def test_pccommon(host):
     result = host.run(cmd)
 
     assert result.rc == 0
-
-    # Tear down - delete all created flavors
-    utility_container = ("lxc-attach -n $(lxc-ls -1 | grep utility | head -n 1) "
-                         "-- bash -c '. /root/openrc ; ")
-
-    flavors = ['m1.tiny', 'm1.small', 'm1.medium', 'm1.large', 'm1.xlarge']
-    for flavor in flavors:
-        cmd = "{} openstack flavor delete {}'".format(utility_container, flavor)
-        host.run(cmd)
